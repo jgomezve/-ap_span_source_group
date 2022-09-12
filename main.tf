0f1f380f668f0a4f5a92cf00181858d0d198c812
jgomezve <jgomezve@cisco.com>
2022-09-12 14:09:51 -0500

    First commit

--- main.tf
+++ main.tf
@@ -1,9 +1,60 @@
-resource "aci_rest_managed" "fvTenant" {
-  dn         = "uni/tn-${var.name}"
-  class_name = "fvTenant"
+resource "aci_rest_managed" "spanSrcGrp" {
+  dn         = "uni/infra/srcgrp-${var.name}"
+  class_name = "spanSrcGrp"
   content = {
-    name      = var.name
-    nameAlias = var.alias
-    descr     = var.description
+    adminSt = var.admin_state == false ? "disabled" : "enabled"
+    descr   = var.description
+    name    = var.name
+  }
+}
+
+resource "aci_rest_managed" "spanSrc" {
+  for_each   = { for source in var.sources : source.name => source }
+  dn         = "${aci_rest_managed.spanSrcGrp.dn}/src-${each.value.name}"
+  class_name = "spanSrc"
+  content = {
+    name        = each.value.name
+    description = each.value.description != null ? each.value.description : ""
+    dir         = each.value.direction != null ? each.value.direction : "both"
+    spanOnDrop  = each.value.span_drop != null ? each.value.span_drop : "false"
+  }
+}
+
+resource "aci_rest_managed" "spanRsSrcToEpg" {
+  for_each   = { for source in var.sources : source.name => source if source.tenant != null && source.application_profile != null && source.endpoint_group != null }
+  dn         = "${aci_rest_managed.spanSrc[each.value.name].dn}/rssrcToEpg"
+  class_name = "spanRsSrcToEpg"
+  content = {
+    tDn = "uni/tn-${each.value.tenant}/ap-${each.value.application_profile_name}/epg-${each.value.endpoint_group_name}"
+  }
+}
+
+resource "aci_rest_managed" "spanRsSrcToL3extOut" {
+  for_each   = { for source in var.sources : source.name => source if source.tenant != null && source.l3out != null }
+  dn         = "${aci_rest_managed.spanSrc[each.value.name].dn}/rssrcToL3extOut "
+  class_name = "spanRsSrcToL3extOut"
+  content = {
+    addr  = "0.0.0.0"
+    encap = "vlan-${each.value.vlan}"
+    tDn   = "uni/tn-${each.value.tenant}/out-${each.value.l3out}"
+  }
+}
+
+resource "aci_rest_managed" "spanRsSrcGrpToFilterGrp" {
+  count      = var.filter_group != null ? 1 : 0
+  dn         = "${aci_rest_managed.spanSrcGrp.dn}/rssrcGrpToFilterGrp"
+  class_name = "spanRsSrcGrpToFilterGrp"
+  content = {
+    tDn = "uni/infra/filtergrp-${var.filter_group}"
+  }
+}
+
+resource "aci_rest_managed" "spanSpanLbl" {
+  dn         = "${aci_rest_managed.spanSrcGrp.dn}/spanlbl-${var.destination.name}"
+  class_name = "spanSpanLbl"
+  content = {
+    descr = var.destination.description != null ? var.destination.description : ""
+    name  = var.destination.name
+    tag   = "yellow-green"
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "name" {
-  description = "Tenant name."
+  description = "SPAN Source Group name."
   type        = string
 
   validation {
@@ -8,19 +8,8 @@ variable "name" {
   }
 }
 
-variable "alias" {
-  description = "Tenant alias."
-  type        = string
-  default     = ""
-
-  validation {
-    condition     = can(regex("^[a-zA-Z0-9_.-]{0,64}$", var.alias))
-    error_message = "Allowed characters: `a`-`z`, `A`-`Z`, `0`-`9`, `_`, `.`, `-`. Maximum characters: 64."
-  }
-}
-
 variable "description" {
-  description = "Tenant description."
+  description = "SPAN Source Group description."
   type        = string
   default     = ""
 
@@ -28,4 +17,46 @@ variable "description" {
     condition     = can(regex("^[a-zA-Z0-9\\!#$%()*,-./:;@ _{|}~?&+]{0,128}$", var.description))
     error_message = "Allowed characters: `a`-`z`, `A`-`Z`, `0`-`9`, `\\`, `!`, `#`, `$`, `%`, `(`, `)`, `*`, `,`, `-`, `.`, `/`, `:`, `;`, `@`, ` `, `_`, `{`, `|`, }`, `~`, `?`, `&`, `+`. Maximum characters: 128."
   }
+}
+
+variable "admin_state" {
+  description = "SPAN Source Group Administrative state."
+  type        = bool
+  default     = false
+}
+
+variable "sources" {
+  description = "List of SPAN sources. Default value `direction`: `both`. Default value `span_drop`: `false`."
+  type = list(object({
+    description         = optional(string)
+    name                = string
+    direction           = optional(bool)
+    span_drop           = optional(bool)
+    tenant              = optional(bool)
+    application_profile = optional(bool)
+    endpoint_group      = optional(bool)
+    l3out               = optional(bool)
+  }))
+  default = []
+
+  validation {
+    condition = alltrue([
+      for s in var.sources : s.description == null || can(regex("^[a-zA-Z0-9\\!#$%()*,-./:;@ _{|}~?&+]{0,128}$", s.description))
+    ])
+    error_message = "`description`: Allowed characters: `a`-`z`, `A`-`Z`, `0`-`9`, `\\`, `!`, `#`, `$`, `%`, `(`, `)`, `*`, `,`, `-`, `.`, `/`, `:`, `;`, `@`, ` `, `_`, `{`, `|`, }`, `~`, `?`, `&`, `+`. Maximum characters: 128."
+  }
+}
+
+variable "filter_group" {
+  description = "SPAN Source Filter Gorup."
+  type        = string
+  default     = null
+}
+
+variable "destination" {
+  description = "SPAN Source Destination Gorup."
+  type = object({
+    description = optional(string)
+    name        = string
+  })
 }
